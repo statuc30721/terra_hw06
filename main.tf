e6a4d468f32de1f42c2d0ac285ab55089b67de9d
statuc30721 <bubalator@yahoo.com>
2024-11-03 15:48:38 +0100

    adding script to main configuration.

--- main.tf
+++ main.tf
@@ -46,10 +46,24 @@ variable env_prefix {
 variable my_ip {
   description = "Identify source IP address allowed to remotely access the virtual machine."
 }
-# 
+
+variable instance_type {
+  description = "Identify instamce type to be used for virtual machine."
+}
+
+variable "public_key_location" {
+  description = "Pass path to the public key to be used to authenticate with a VM instance."
+  
+}
+
+variable "private_key_location" {
+  description = "Pass path to the private key to be used to authenticate with a VM instance."
+  
+}
+
 #----------------------------------------------------------------#
 # 
-# Resources
+# Virtual Private Cloud and Network Setup
 
 # Create a Virtual Private Cloud. 
 resource "aws_vpc" "myapp-vpc" {
@@ -124,5 +138,89 @@ resource "aws_default_security_group" "default-sg" {
   tags = {
     Name: "${var.env_prefix}-default-sg"
   }
-
 }
+
+#----------------------------------------------------------------#
+# 
+# Create an AWS EC2 Virtual Machine Instance. 
+
+# Retrieve the latest AMI
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values = ["al2023-ami-2023*-x86_64"]
+  }
+
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+
+  }
+}
+
+output "aws-ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "ec2-public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+# Identify VPC, network and security group for Linux virtual machine.
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = file(var.public_key_location)
+  
+}
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+
+# Identify the name of the SSH key pair to be associated with your linux VM
+# NOTE: This SSH key requires a public and private key pair that you have access to.
+#
+# You must provide the full path to the public and private key pair you intend to use.
+# For example a user named joe on a linux system would typically be /home/joe/.ssh/id_rsa.pub 
+# for their public key and /home/joe/.ssh/id_rsa for the private key. 
+  key_name = aws_key_pair.ssh-key.key_name
+
+#----------------------------------------------------------------#
+# Install software on the Amazon EC2 Instance.
+
+user_data = <<EOF
+                #!/bin/bash
+                sudo yum update -y && sudo yum install -y docker
+                sudo systemctl start docker
+                sudo usermod -aG docker ec2-user
+                docker run -p 8080:80 nginx
+              EOF
+
+user_data_replace_on_change = true
+
+
+
+
+tags = {
+    Name: "${var.env_prefix}-server"
+  }
+}
+
+
+
+
+
