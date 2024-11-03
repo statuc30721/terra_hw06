f6ca9d5951aeb2021dd8318a0fd677e43f264b06
statuc30721 <bubalator@yahoo.com>
2024-11-03 16:54:48 +0100

    updated main config file

--- main.tf
+++ main.tf
@@ -206,26 +206,6 @@ user_data = file("entry-script.sh")
 
 user_data_replace_on_change = true
 
-/*connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ec2-user"
-    private_key = file(var.private_key_location)
-}*/
-
-/*provisioner "file" {
-  source = "entry-script.sh"
-  destination = "/home/ec2-user/entry-script-on-ec2.sh"
-}*/
-
-/*provisioner "remote-exec" {
-  inline = ["/home/ec2-user/entry-script-on-ec2.sh"]
-}*/
-
-/*provisioner "local-exec" {
-  command = "echo ${self.public_ip} > output.txt"
-}*/
-
 tags = {
     Name: "${var.env_prefix}-server"
   }
