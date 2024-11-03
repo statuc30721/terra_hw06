0f56721d0fc1979e834d1871d133a1b445f515ff
statuc30721 <bubalator@yahoo.com>
2024-11-03 12:39:29 +0100

    updated main config with additional variables

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ provider "aws" {
   
 }
 
+#----------------------------------------------------------------#
+#
+# Variables
+
 # Variable aws_region is used to provide a default
 # AWS region. You can modify this file directly or 
 # for portability put this in a terraform variable file.
@@ -15,19 +19,17 @@ variable "aws_region" {
 }
 
 # This provides the CIDR blocks for VPC and subnets.
-# I use a variable file versus command line to make it easier to 
-# deploy. 
 
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
 }
 
+# This variable sets the subnet within the CIDR block. 
 variable "subnet_cidr_block" {
   
 }
 
-# The default value can be overriden in a variable file.
-
+# Set AWS availaibility zone for the VPC. 
 variable "avail_zone" {
     description = "AWS region availability zone"
     type = string
@@ -35,9 +37,30 @@ variable "avail_zone" {
   
 }
 
+# Identify the virtual environment. For example Development,
+# Test, etc. 
 variable env_prefix {
-  description = "Use to identify for deployment."
+  description = "Use to identify for deployment environment."
 }
+
+#----------------------------------------------------------------#
+# 
+# Resources
+
+# Setup additional routes within the VPC. 
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+}
+
 
 # Create a Virtual Private Cloud. 
 resource "aws_vpc" "myapp-vpc" {
